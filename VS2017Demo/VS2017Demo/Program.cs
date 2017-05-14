629f689c033f9b11e1fb51f58187afe31ae158b0
Robert Haken <haken@havit.cz>
2017-05-15 01:29:08 +0200

    Live Unit Testing, MSTest v2, Exception Helper

---
+++ VS2017Demo/VS2017Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS2017Demo
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+
+	public class Calculator
+	{
+		public decimal Add(int value1, int value2)
+		{
+			return value1 + value2;
+		}
+
+		public decimal Sub(int value1, int value2)
+		{
+			return value1 - value2;
+		}
+
+		public int Compute(object obj1, object obj2)
+		{
+			return obj1.GetHashCode() + obj2.GetHashCode();
+		}
+	}
+
+}
---
+++ VS2017Demo/VS2017DemoTests/CalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VS2017Demo;
+
+namespace VS2017DemoTests
+{
+	// MSTest v2
+	// Install-Package MSTest.TestFramework
+	// Install-Package MSTest.TestAdapter
+
+	[TestClass]
+	public class CalculatorTests
+	{
+		[DataTestMethod]
+		[DataRow(5, 5, 10)]
+		[DataRow(0, 0, 0)]
+		[DataRow(-1, -5, -6)]
+		public void Calculator_Add(int value1, int value2, int expected)
+		{
+			// arrange
+			var calculator = new Calculator();
+
+			// act
+			var result = calculator.Add(value1, value2);
+
+			// assert
+			Assert.AreEqual(expected, result);
+		}
+
+		[TestMethod]
+		public void Calculator_Compute_Integers()
+		{
+			// arrange
+			var calculator = new Calculator();
+
+			// act
+			var result = calculator.Compute(1, 1);
+
+			// assert
+			Assert.AreEqual(2, result);
+		}
+
+		[TestMethod]
+		public void Calculator_Compute_Null()
+		{
+			// arrange
+			var calculator = new Calculator();
+
+			// act
+			var result = calculator.Compute(1, null);
+
+			// assert
+			Assert.AreEqual(2, result);
+		}
+	}
+}
